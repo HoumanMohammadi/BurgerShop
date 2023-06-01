25dd877b47644e50f1565235e9ba3bad22a5ad29
Houman Mohammadi <houmane.el@gmail.com>
2023-06-01 17:42:08 +0200

    add menus integerationtest added

--- src/test/java/de/iav/burgershop/controller/MenuControllerIntegerationTest.java
+++ src/test/java/de/iav/burgershop/controller/MenuControllerIntegerationTest.java
@@ -1,10 +1,14 @@
 package de.iav.burgershop.controller;
 
+import de.iav.burgershop.model.Beverage;
+import de.iav.burgershop.model.Food;
+import de.iav.burgershop.model.Menu;
 import de.iav.burgershop.repository.MenuRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -30,12 +34,60 @@ class MenuControllerIntegerationTest {
     }
 
     @Test
-    void getSingleMenu() {
+    void getSingleMenu_whenAMenu_thenExpectStatusOKAndOneMenu() throws Exception {
+        Menu menu=new Menu("M01","Menus1",25,
+                new Food("cheese burger"),
+                new Food("sweet potato"),
+                new Beverage("Cola"));
+        menuRepository.addMenu(menu);
+
+        final String searchID= menu.id();
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/menus/{id}",searchID))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(
+                        """
+                                        {
+                                            "id":"M01",
+                                            "name":"Menus1",
+                                            "price":25,
+                                            "mainDish":{"name":"cheese burger"},
+                                            "sideDish":{"name":"sweet potato"},
+                                            "beverage":{"name":"Cola"}
+                                        }
+                                        """
+                ));
     }
 
     @Test
-    void addMenu() {
+    void addMenu_WhenAMenuAvailable_thenStatusNotOK() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/menus")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                        """
+                                        {
+                                            "id":"M01",
+                                            "name":"Menus1",
+                                            "price":25,
+                                            "mainDish":{"name":"cheese burger"},
+                                            "sideDish":{"name":"sweet potato"},
+                                            "beverage":{"name":"Cola"}
+                                        }
+                                        """
+                )).andExpect(MockMvcResultMatchers.status().isOk());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/menus"))
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value("M01"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Menus1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].price").value(25))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].mainDish.name").value("cheese burger"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].sideDish.name").value("sweet potato"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].beverage.name").value("Cola"));
+
     }
+
 
     @Test
     void changeMenuByID() {
