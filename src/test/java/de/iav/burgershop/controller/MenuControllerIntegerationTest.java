6feb4999bdfeaf4598100be5474ca799489de20a
Houman Mohammadi <houmane.el@gmail.com>
2023-06-01 16:05:02 +0200

    all menus integerationtest added

--- src/test/java/de/iav/burgershop/controller/MenuControllerIntegerationTest.java
+++ src/test/java/de/iav/burgershop/controller/MenuControllerIntegerationTest.java
@@ -1,11 +1,32 @@
 package de.iav.burgershop.controller;
 
+import de.iav.burgershop.repository.MenuRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 
 class MenuControllerIntegerationTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private MenuRepository menuRepository;
+
     @Test
-    void allMenus() {
+    void allMenus_WhenNoMenus_thenExpectStatusOkAndEmptyList() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/menus"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("""
+[]
+"""));
+
     }
 
     @Test
